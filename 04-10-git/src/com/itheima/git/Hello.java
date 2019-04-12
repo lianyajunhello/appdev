fd7cc0bffbf86b4f44d2b9b7190f3772e6c2d54b
lianyajun <1159687861@qq.com>
2019-04-12 14:26:01 +0800

    update first commit

--- 04-10-git/src/com/itheima/git/Hello.java
+++ 04-10-git/src/com/itheima/git/Hello.java
@@ -1,7 +1,12 @@
 package com.itheima.git;
 
 public class Hello {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("hello git project");
+        show();
+    }
+
+    public static void show() {
+        System.out.println("展示");
     }
 }
